252c657c78c23d58d41b6629aa545d04da586e50
hossam ali <hossemedine@outlook.com>
2022-11-09 21:37:41 +0100

    add sending verification email on signup

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -116,7 +116,6 @@ export const authOptions: NextAuthOptions = {
         session.user={
           ...user,
           ...token,
-          emailisverfied:user.emailisverfied
         }  
       }
       
