8219cf99377c63b45fd25b57a0242c6968389bab
hossam ali <hossemedine@outlook.com>
2022-11-11 01:43:06 +0100

    redirect the user after verifiyng the email

--- src/pages/verify/[...id].tsx
+++ src/pages/verify/[...id].tsx
@@ -1,7 +1,7 @@
 import { trpc } from "../../utils/trpc";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import { useLanguage } from "../../store/store";
+import { useLanguage, useSignInModal } from "../../store/store";
 
 
 
@@ -12,6 +12,7 @@ const Verify  = () => {
     const id=router.query.id
     const verify=trpc.verfy.verify.useMutation()
     const Language=useLanguage()
+    const signInmodal=useSignInModal()
  
     useEffect(()=>{
        if(typeof window != 'undefined'){if(!id){
@@ -22,16 +23,20 @@ const Verify  = () => {
        }}
 
     },[id])
- 
-console.log(verify.data)
 
+
+ 
+  const hundelSignIn=()=>{
+        signInmodal.toggleShow()
+        router.push('/')
+  }
     return ( <div className="py-10">
         <h1 className="text-center text-2xl md:text-3xl">{Language.lng=='ENG'?'Email verification':"Verification de l'email "}</h1>
         {verify.isLoading&&<p className="text-center">{Language.lng=='ENG'?'Please wait while we verify your email':'Patienter pendant que nous verifions votre email '}</p>}
         {verify.isError&&<p className="text-center mt-10">{verify.error.message}</p>}
         {verify.data&&<div className="text-center">
             <p className="mt-5">{Language.lng=='ENG'?'Your email is now verified ':'Votre email est verifié'} </p> 
-            <button className=" mx-10 p-1 rounded-md bg-primary1 mt-5" >{Language.lng=='ENG'?'Login':"S'identifier"}</button>
+            <button onClick={hundelSignIn}  className=" mx-10 p-1 rounded-md bg-primary1 mt-5" >{Language.lng=='ENG'?'SignIn':"S'identifier"}</button>
         </div>}
     </div> );
 }
