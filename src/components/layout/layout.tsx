c92e5dd21e509201969147157e5627b830f9990f
hossam ali <hossemedine@outlook.com>
2022-11-14 00:36:14 +0100

    remove toggle button

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -39,7 +39,7 @@ const Layout :React.FC<porps>= (props)=> {
             {props.children}
                     {notification.show&&<Notification/>}
 
-                    <button onClick={notification.toggleShow} className="mx-auto">toggle</button>
+                   
                         </main>
 
         <Footer/>
